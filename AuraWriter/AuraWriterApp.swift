91e7e7d7a1abc6ce014dbbe04c5d988a3da520ff
Trong Nguyen <lidroider@gmail.com>
2026-05-01 11:09:44 +0700

    Initial Commit

---
+++ AuraWriter/AuraWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraWriterApp.swift
+//  AuraWriter
+//
+//  Created by Trong Nguyen on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
